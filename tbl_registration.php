4460ea385886f520046ff3e42ae604647a72ff81
teamkalf <kalifasanneh@gmail.com>
2020-10-25 21:02:22 +0000

    styling page layout

--- tbl_registration.php
+++ tbl_registration.php
@@ -8,21 +8,23 @@
 </head>
 <body>
     <div class="tbl_registration_main">
-        <div class="tbl_registration_navbar">
-            <div class="tbl_registration_logo">
-                <h3 class="tbl_registration_logo">nesthet app</h3>
+        <div class="tbl_registration_sub">
+            <div class="tbl_registration_navbar">
+                <div class="tbl_registration_logo">
+                    <h3 class="tbl_registration_logo">nesthet app</h3>
+                </div>
+                <div class="tbl_registration_navbar-search">
+                    <a href="mdi_parent.php" class="tbl_registration_search-nav">home</a>
+                    <input type="text" name="search" class="form_input" placeholder="search">
+                    <button class="tbl_registration_navbar-search-btn">search</button>
+                </div>
             </div>
-            <div class="tbl_registration_navbar-search">
-                <a href="mdi_parent.php" class="tbl_registration_search-nav">home</a>
-                <input type="text" name="search" class="form_input" placeholder="search">
-                <button class="tbl_registration_navbar-search-btn">search</button>
+            <div class="tbl_registration_header">
+                <h1 class="tbl_registration_header">records on registration table</h1>
             </div>
-        </div>
-        <div class="tbl_registration_header">
-            <h1 class="tbl_registration_header">records on registration table</h1>
-        </div>
-        <div class="tbl_registration_data">
+            <div class="tbl_registration_data">
 
+            </div>
         </div>
     </div>
 </body>
