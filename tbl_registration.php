6fdd5a1e6369c131aa2610a9f2c9bad605b79b5b
teamkalf <kalifasanneh@gmail.com>
2020-10-25 20:56:35 +0000

    designing the registration table

---
+++ tbl_registration.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registration table</title>
+    <link rel="stylesheet" href="./sass/main.css">
+</head>
+<body>
+    <div class="tbl_registration_main">
+        <div class="tbl_registration_navbar">
+            <div class="tbl_registration_logo">
+                <h3 class="tbl_registration_logo">nesthet app</h3>
+            </div>
+            <div class="tbl_registration_navbar-search">
+                <a href="mdi_parent.php" class="tbl_registration_search-nav">home</a>
+                <input type="text" name="search" class="form_input" placeholder="search">
+                <button class="tbl_registration_navbar-search-btn">search</button>
+            </div>
+        </div>
+        <div class="tbl_registration_header">
+            <h1 class="tbl_registration_header">records on registration table</h1>
+        </div>
+        <div class="tbl_registration_data">
+
+        </div>
+    </div>
+</body>
+</html>
